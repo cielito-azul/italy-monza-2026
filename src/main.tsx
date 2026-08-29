ac1960baf9b747f225fbd5711de592f7a294ea8e
cielito-azul <claire.nzcc@gmail.com>
2026-08-29 08:53:39 +0800

    Add mobile app icon and install support

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>,
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/italy-monza-2026/sw.js");
+  });
+}
